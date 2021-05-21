4a5dd318e187d0ce0217460398c260b0f4fe86e3
de-sage <gomotosho1@gmail.com>
2021-05-21 23:10:05 +0100

    clean up

--- src/main/java/com/claritae/maintainance/repository/MaintainaceRecordRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.claritae.maintainance.repository;
-//
-//import com.claritae.maintainance.model.equipmentrecord.MaintainaceRecord;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.data.repository.NoRepositoryBean;
-//
-//@NoRepositoryBean
-//public interface MaintainaceRecordRepository extends CrudRepository<MaintainaceRecord, Long> {
-//}
